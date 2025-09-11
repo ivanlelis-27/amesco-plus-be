579ea45b43ee3ba7e9d32cb128368702891df151
ivanlelis-27 <ezekielivan.lelis@avanza.ph>
2025-09-11 15:14:36 +0800

    ignored file with smtp credentials

--- AmescoAPI/obj/Debug/net10.0/AmescoAPI.AssemblyInfo.cs
+++ AmescoAPI/obj/Debug/net10.0/AmescoAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AmescoAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64ca18eafc4e200f2495693a086a7ce7a303f2af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79a89f20e3cfd702b524a4189046de15964623f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("AmescoAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AmescoAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
